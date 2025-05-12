1a432c9c46e523cc94acf66455183f6dc14f2715
eemzain <hudazain55@gmail.com>
2025-05-12 09:13:01 +0300

    .

--- lib/final_report/src/domain/final_report_service.dart
+++ lib/final_report/src/domain/final_report_service.dart
@@ -591,8 +591,9 @@ class FinalReportService extends FinalReportRepository {
           double totalAmount = amount;
           if (journalType == 'cash') {
             // totalAmount -= invoice['change'] ?? 0.0;
-            print("journalType:: $totalAmount");
+            print("journalType:: $totalAmount ${invoice['change']}");
             totalAmount  = totalAmount - (invoice['change'] ?? 0.0);
+            print("totalAmount:: $totalAmount");
           }
           if (resultMap.containsKey(paymentId)) {
             resultMap[paymentId]!['total_amount'] += totalAmount;
